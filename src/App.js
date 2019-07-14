dfa73221b1b8ab304c1cb22cea042b0c5d7f5d94
Agridho Ilhamantama <agridhoi@gmail.com>
2019-07-14 07:43:52 +0200

    add some features and connect to back-end

--- src/App.js
+++ src/App.js
@@ -36,8 +36,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignIn: false
+      isSignIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculatefaceLocation = (data) => {
@@ -67,7 +84,22 @@ class App extends Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculatefaceLocation(response)))
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+      }
+      this.displayFaceBox(this.calculatefaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
@@ -81,7 +113,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignIn, imageUrl, route, box } = this.state; 
+    const { isSignIn, imageUrl, route, box, } = this.state; 
     return (
       <div className="App">
         <Particles 
@@ -91,18 +123,24 @@ class App extends Component {
         { route === 'home' 
           ? <div> 
               <Logo />
-              <Rank />
+              <Rank 
+                name={this.state.user.name} 
+                entries={this.state.user.entries} 
+              />
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
-                onButtonSubmit={this.onButtonSubmit} />
+                onButtonSubmit={this.onButtonSubmit} 
+              />
               <FaceRecognition 
                 box={box} 
-                imageUrl={imageUrl} />
+                imageUrl={imageUrl} 
+              />
             </div>
           : (
             route === 'signin' 
-            ? <Signin onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange} />
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} 
+              />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             )      
         }
       </div>
